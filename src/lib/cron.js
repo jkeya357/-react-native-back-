0c2f17993149d6c51022564d7106a7f4ab19ec4a
Joshua <jkeya357@gmail.com>
2025-11-05 15:45:26 +0200

    fixing the env variable

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -5,7 +5,7 @@ const job = new cron.CronJob("*/14 * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
       if (res.statusCode === 200) console.log("GET request sent successfully");
-      else console.log("GET request failed", res.statusCode);
+      else console.log("GET request failed", res.statusCode, res.error);
     })
     .on("error", (e) => console.error("Error while sending request", e));
 });
